b53d91af2700a0cb3cde4c8a3c7c06dff423033f
Anurag Dubey <anuragdubey26202@gmail.com>
2025-01-09 21:04:50 +0530

    Add files via upload

---
+++ Basics of C programing/fristProgramInC.c
@@ -0,0 +1,12 @@
+#include<stdio.h> // Header files in C provide declarations for functions, macros, and constants. 
+                  /* They allow code reuse and modular programming by enabling separate compilation 
+                   of definitions (in source files) and their usage across multiple files.*/
+
+int main()        /* The `int main` function is the entry point of a C program. 
+                      It is where program execution starts and typically returns an integer 
+                      to indicate success (0) or error codes to the operating system.*/
+{
+    printf("Hello C Programing"); // printf() is use to print the values , statement etc
+
+    return 0;
+}
